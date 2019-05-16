ff461e0aa7cfda665e684abeb02fd22376ec26f6
Tim Bendt <sirtimbly@gmail.com>
2019-05-15 21:25:56 -0600

    added frets-styles-generator successfully

--- src/views/root.ts
+++ src/views/root.ts
@@ -1,6 +1,8 @@
-import { app } from "../app";
-import { h } from "maquette";
+import { app } from '../app'
+import { $ } from '../styles/app-styles'
 
 export const renderRoot = (app: app) => {
-  return h('div.max-w-sm.mx-auto.flex.p-6.bg-white.rounded-lg.shadow-xl', [`Hello ${app.modelProps.username}`])
+  return $.div.maxWSm.mxAuto.flex.p_6.bgWhite.rounded.shadowXl.textBlue_600.m_3.h([
+    `Hello ${app.modelProps.username}`
+  ])
 }
