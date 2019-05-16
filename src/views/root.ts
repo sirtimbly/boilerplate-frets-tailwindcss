f6c7f634786451597bd5870114229cd1b9eb2937
Tim Bendt <sirtimbly@gmail.com>
2019-05-15 21:17:26 -0600

    tailwind integrated with parcel

--- src/views/root.ts
+++ src/views/root.ts
@@ -2,5 +2,5 @@ import { app } from "../app";
 import { h } from "maquette";
 
 export const renderRoot = (app: app) => {
-  return h('div', [`Hello ${app.modelProps.username}`])
+  return h('div.max-w-sm.mx-auto.flex.p-6.bg-white.rounded-lg.shadow-xl', [`Hello ${app.modelProps.username}`])
 }
